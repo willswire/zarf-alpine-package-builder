39c9e679a712d2afa8a338325c4b192e45033879
William Walker <w_walker@icloud.com>
2023-04-30 22:53:14 +0000

    update setup script

--- script.sh
+++ script.sh
@@ -1,5 +1,12 @@
 #!/bin/ash
 
+# install the alpien build sdk
+sudo apk add alpine-sdk
+
+# generate signing keys
+abuild-keygen -a -i
+
 # subshell with proper groups in devcontainer
+sudo addgroup vscode abuild
 exec sudo su -l $USER
 cd /workspaces/apk-add-zarf/zarf
